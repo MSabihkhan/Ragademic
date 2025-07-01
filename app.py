357e587de68fc3dcaaf9c3eb42395a879098e4fb
MSabihkhan <l230654@lhr.nu.edu.pk>
2025-07-02 00:16:58 +0500

    Sqlite issues resolution try 1

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from ingestion.loader import save_uploaded_files
 from Vectorstore.index import get_index_from_chroma
